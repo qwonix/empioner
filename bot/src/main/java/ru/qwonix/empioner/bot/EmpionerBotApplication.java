e70791c1fa01d17d9adb4f90ca53b8d8fb21b293
qwonix <roman.qwonix@gmail.com>
2024-11-26 08:03:23 +0300

    init commit

---
+++ bot/src/main/java/ru/qwonix/empioner/bot/EmpionerBotApplication.java
@@ -0,0 +1,13 @@
+package ru.qwonix.empioner.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpionerBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpionerBotApplication.class, args);
+    }
+
+}
---
+++ bot/src/test/java/ru/qwonix/empioner/bot/EmpionerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.qwonix.empioner.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpionerBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
