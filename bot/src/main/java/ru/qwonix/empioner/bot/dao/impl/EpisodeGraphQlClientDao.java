e777bdbd25dc5e93e50641e0112d4f3059d65417
qwonix <roman.qwonix@gmail.com>
2025-02-06 23:55:18 +0300

    use service app via GraphQL API as dao

---
+++ bot/src/main/java/ru/qwonix/empioner/bot/dao/impl/EpisodeGraphQlClientDao.java
@@ -0,0 +1,120 @@
+package ru.qwonix.empioner.bot.dao.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.client.GraphQlClient;
+import org.springframework.stereotype.Repository;
+import ru.qwonix.empioner.bot.dao.EpisodeDao;
+import ru.qwonix.empioner.bot.entity.Episode;
+import ru.qwonix.empioner.bot.entity.id.EpisodeId;
+import ru.qwonix.empioner.bot.entity.id.SeasonId;
+import ru.qwonix.empioner.bot.entity.id.VideoGroupId;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class EpisodeGraphQlClientDao implements EpisodeDao {
+
+    private static final String GET_BY_ID_QUERY = """
+            query GetEpisodeById($id: EpisodeId!) {
+                getEpisodeById(id: $id) {
+                    id
+                    title
+                    description
+                    number
+                    previousEpisodeId
+                    nextEpisodeId
+                    videoGroupId
+                    seasonId
+                    imageId
+                    isAvailable
+                }
+            }
+            """;
+    private static final String COUNT_BY_SEASON_QUERY = """
+            query CountEpisodesBySeasonId($id: SeasonId!) {
+                countEpisodesBySeasonId(id: $id)
+            }
+            """;
+    private static final String FIND_BY_SEASON_QUERY = """
+            query GetEpisodesBySeasonId($id: SeasonId!, $limit: Int, $page: Int) {
+                getEpisodesBySeasonId(id: $id, limit: $limit, page: $page) {
+                    id
+                    title
+                    description
+                    number
+                    previousEpisodeId
+                    nextEpisodeId
+                    videoGroupId
+                    seasonId
+                    imageId
+                    isAvailable
+                }
+            }
+            """;
+    private static final String FIND_BY_VIDEO_GROUP_QUERY = """
+            query GetEpisodeByVideoGroupId($id: VideoGroupId!) {
+                getEpisodeByVideoGroupId(id: $id) {
+                    id
+                    title
+                    description
+                    number
+                    previousEpisodeId
+                    nextEpisodeId
+                    videoGroupId
+                    seasonId
+                    imageId
+                    isAvailable
+                }
+            }
+            """;
+    private final GraphQlClient graphQlClient;
+
+    @Override
+    public Optional<Episode> findById(EpisodeId id) {
+        return graphQlClient.document(GET_BY_ID_QUERY)
+                .variable("id", id.value().toString())
+                .retrieve("getEpisodeById")
+                .toEntity(Episode.class)
+                .blockOptional();
+    }
+
+    @Override
+    public Integer countAllBySeasonId(SeasonId id) {
+        return graphQlClient.document(COUNT_BY_SEASON_QUERY)
+                .variable("id", id.value().toString())
+                .retrieve("countEpisodesBySeasonId")
+                .toEntity(Integer.class)
+                .defaultIfEmpty(0)
+                .block();
+    }
+
+    @Override
+    public Integer countAllAvailableBySeasonId(SeasonId seasonId) {
+        return this.countAllBySeasonId(seasonId);
+    }
+
+    @Override
+    public List<Episode> findAllBySeasonIdOrderByNumberWithLimitAndPage(SeasonId id, int limit, int page) {
+        return graphQlClient.document(FIND_BY_SEASON_QUERY)
+                .variables(Map.of(
+                        "id", id.value().toString(),
+                        "limit", limit,
+                        "page", page
+                ))
+                .retrieve("getEpisodesBySeasonId")
+                .toEntityList(Episode.class)
+                .block();
+    }
+
+    @Override
+    public Optional<Episode> findByVideoGroupId(VideoGroupId id) {
+        return graphQlClient.document(FIND_BY_VIDEO_GROUP_QUERY)
+                .variable("id", id.value().toString())
+                .retrieve("getEpisodeByVideoGroupId")
+                .toEntity(Episode.class)
+                .blockOptional();
+    }
+}
---
+++ bot/src/main/java/ru/qwonix/empioner/bot/dao/impl/ImageGraphQlClientDao.java
@@ -0,0 +1,39 @@
+package ru.qwonix.empioner.bot.dao.impl;
+
+import org.springframework.graphql.client.GraphQlClient;
+import org.springframework.stereotype.Repository;
+import ru.qwonix.empioner.bot.dao.ImageDao;
+import ru.qwonix.empioner.bot.entity.Image;
+import ru.qwonix.empioner.bot.entity.id.ImageId;
+
+import java.util.Optional;
+
+@Repository
+public class ImageGraphQlClientDao implements ImageDao {
+
+    private static final String GET_TELEGRAM_FILE_ID_QUERY = """
+            query GetImageTelegramFileId($id: ImageId!) {
+                getImageTelegramFileId(id: $id) {
+                    id
+                    telegramFileId
+                    telegramFileUniqueId
+                }
+            }
+            """;
+    private final GraphQlClient graphQlClient;
+
+    public ImageGraphQlClientDao(GraphQlClient graphQlClient) {
+        this.graphQlClient = graphQlClient;
+    }
+
+    @Override
+    public Optional<Image> findTelegramFileIdByImageId(ImageId id) {
+        return Optional.ofNullable(
+                graphQlClient.document(GET_TELEGRAM_FILE_ID_QUERY)
+                        .variable("id", id.value())
+                        .retrieve("getImageTelegramFileId")
+                        .toEntity(Image.class)
+                        .block()
+        );
+    }
+}
